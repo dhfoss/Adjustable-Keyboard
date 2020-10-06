640e3f9a3cc8d006c58b3d7e0c18a53cc7c5b8bb
Daniel Hawthorne-Foss <dhfoss89@gmail.com>
2020-10-06 15:31:31 -0400

    A3 and A4 keys work in a limited way

--- script.js
+++ script.js
@@ -11,7 +11,113 @@ $(document).ready(function() {
         $(this).toggleClass('bg-warning');
     });
 
+
+    // var audioContext = new AudioContext();
+    // var filter = audioContext.createBiquadFilter();
+    // var oscillator = audioContext.createOscillator();
+
+    // oscillator.connect(filter);
+    // filter.connect(audioContext.destination);
+
+    // oscillator.start(audioContext.currentTime);
+
+    // var aContext;
+    // var osc;
+
+    // var aContext;
+    // var osc;
+
+    var aContext;
+    var osc;
+    $('#A4').on('mousedown', function() {
+        if ($('#A4').hasClass('off')) {
+            aContext = new window.AudioContext();
+            osc = aContext.createOscillator();
+            osc.connect(aContext.destination);
+            osc.start();
+            $('#A4').removeClass('off');
+            $('#A4').addClass('on');
+        } else
+        if ($('#A4').hasClass('on')) {
+            osc.stop();
+            $('#A4').removeClass('on');
+            $('#A4').addClass('off');
+        }
+    });
+
+
+
+
+    $('#A3').on('mousedown', function() {
+        if ($('#A3').hasClass('off')) {
+            aContext = new window.AudioContext();
+            osc = aContext.createOscillator();
+            osc.connect(aContext.destination);
+            osc.frequency.value = "220";
+            osc.start();
+            $('#A3').removeClass('off');
+            $('#A3').addClass('on');
+        } else
+        if ($('#A3').hasClass('on')) {
+            osc.stop();
+            $('#A3').removeClass('on');
+            $('#A3').addClass('off');
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //  Start and stop A 440
+        // if ( $('#A4').hasClass('off') ) {
+
+        //     $('#A4').on('mousedown', function() {
+        //         aContext = new window.AudioContext();
+        //         osc = aContext.createOscillator();
+        //         osc.connect(aContext.destination);
+        //         osc.start();
+        //         $(this).toggleClass('bg-danger');
+
+        //         $('#A4').removeClass('off');
+        //         $('#A4').addClass('on');
+        //         $('#A4').off();
+        //         $('#A4').on('mousedown', function() {
+        //             if ( $('#A4').hasClass('on') ) {
+        //                 osc.stop();
+        //                 $(this).toggleClass('bg-danger');
+
+        //                 $('#A4').removeClass('on');
+        //                 $('#A4').addClass('off');
+        //                 $('#A4').off();
+        //             }
+        //         })
+        //     });
+        // }
+        
+
+
+
+        
+
+    
+
+
+
 });
+
+
+
 
 // Same as above, but without jQuery.
     // For this to work, the bg-color needs to be updated in html class (bootstrap).
